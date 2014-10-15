cda3c8d0eec332f979559d173c81c8a1588ee842
Thiago <tdsimao@gmail.com>
2014-10-15 15:57:46 -0300

    fisrt tests, BinaryTree search

--- tree/tree.rb
+++ tree/tree.rb
@@ -1,4 +1,7 @@
+#!/usr/bin/ruby -w
 class Node
+    attr_accessor :content
+    attr_accessor :value
     def initialize content, value
         @content = content
         @value = value
@@ -15,13 +18,16 @@ class Node
             c.puts
         end
     end
+    
+    
+    
 end
 
 
-class Heap < Node
-        
+class BinaryTree < Node
+    
     def add node
-        if node.value < @value
+        if node.value >= @value
             add_greater node
         else
             add_lesser node
@@ -44,24 +50,65 @@ class Heap < Node
         end
     end
     
-    def puts
-        if @children[0].nil?
-            self.puts @children[0]
+    
+    
+    
+    def search key
+        #return node with value equals the given key
+        if key == @value 
+            return self
+        elsif key < @value
+            if not @children[0].nil?
+                @children[0].search key
+            else
+                return nil
+            end
+        else 
+            if not @children[1].nil?
+                @children[1].search key
+            else
+                return nil
+            end
         end
-        puts @value
-        if @children[1].nil?
-            self.puts @children[1]
-        end
-        
     end
+    
+    
+    def as_s
+        
+        aux = ''
+        #aux << "menores\n"
+        if not @children[0].nil?
+            aux << @children[0].as_s 
+        end
+        #aux << "eu\n"
+        aux << "#{@content}" << "\n"
+        
+        #aux << "maiores\n"
+        if not @children[1].nil?
+             aux << @children[1].as_s
+        end
+        return aux
+    end
+    
+    
+    
 end
             
             
         
-root = Node.new 'root', 0
-
-10.times do |i|
-    puts i
+root = BinaryTree.new 'root', 0
+numbers = [1,3,4,-1,  2]
+numbers.each do |i|
+    node = BinaryTree.new "node#{i}", i
+    root.add node
 end
+
+# puts root.as_s
+# 
+#  root.search(2).content
+# puts root.search(0).content
+# puts root.search(-1).content
+# puts root.search(10)
+
 
 
